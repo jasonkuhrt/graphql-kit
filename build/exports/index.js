27e67a814dd9a96446a53bca611d65e107b6fc65
Jason Kuhrt <jasonkuhrt@me.com>
2025-09-16 14:22:44 -0400

    feat: export namespaces from root, sub-exports open barrel modules

--- build/exports/index.js
+++ build/exports/index.js
@@ -1,2 +1,17 @@
-export const hello = 'world';
+// Namespace exports
+export { Catalog } from '../catalog/$.js';
+export { Change } from '../change/$.js';
+export { DateOnly } from '../date-only/$.js';
+export { Grafaid } from '../grafaid/$.js';
+export { GrafaidOld } from '../grafaid-old/$.js';
+export { GraphQLError } from '../graphql-error/$.js';
+export { GraphqlSchemaLoader } from '../graphql-schema-loader/$.js';
+export { GraphQLSchemaPath } from '../graphql-schema-path/$.js';
+export { Lifecycles } from '../lifecycles/$.js';
+export { Revision } from '../revision/$.js';
+export { Schema } from '../schema/$.js';
+export { SchemaDefinition } from '../schema-definition/$.js';
+export { Version } from '../version/$.js';
+export { VersionCoverage } from '../version-coverage/$.js';
+// kit-temp doesn't have a namespace export, access via graphql-kit/kit-temp
 //# sourceMappingURL=index.js.map
